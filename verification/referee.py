b65e2cce21cc04f7d081e531ffdd4d299acead60
Oleksandr Liabakh <oleksandrliabakh@Oleksandrs-MacBook.local>
2016-07-19 15:01:16 +0300

    js version

--- verification/referee.py
+++ verification/referee.py
@@ -50,7 +50,7 @@ api.add_listener(
         },
         function_name={
             "python": "broken_clock",
-            "js": "brokenClock"
+            "js": "brokenСlock"
         }
         # checker=None,  # checkers.float.comparison(2)
         # add_allowed_modules=[],
